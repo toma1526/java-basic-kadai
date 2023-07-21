48afb022e0d98f9b512b97631361b56d059be5c2
toma1526 <134501930+toma1526@users.noreply.github.com>
2023-07-21 11:44:47 +0900

    kadai_004にScore _Chapter04.javaをアップロード

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,15 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+	public static void main(String[] args) {
+
+		//全員のテストの点数の合計
+		int total = 10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100;
+		System.out.println("合計点数" + total + "点");
+
+		//10人の平均点
+		System.out.println("平均点" + (total / 10) + "点");
+
+	}
+
+}
