7fb0416299f9f638382defe251d3898f31d7279f
toma1526 <134501930+toma1526@users.noreply.github.com>
2023-07-21 10:46:07 +0900

    kadai_004

---
+++ Score_Chapter04.java
@@ -0,0 +1,17 @@
+package kadai_004;
+
+public class Score_Chapter04{
+	public static void main(String[]args) {
+		
+		//全員のテストの点数の合計
+		int total =1+2+3;
+	System.out.println("合計点数"+total+"点");
+	
+	
+	  //10人の平均点
+	System.out.println("平均点"+(total/3)+"点");
+		
+
+	}
+	
+}
